a4bfe10d086cc37992cb0442b9d32f089105be06
unknown <erdoganabaci97@gmail.com>
2019-06-27 22:15:09 +0300

    bottom fragment eklendi

--- app/src/main/java/app/deverdo/instagram/MainActivity.java
+++ app/src/main/java/app/deverdo/instagram/MainActivity.java
@@ -1,13 +1,69 @@
 package app.deverdo.instagram;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import app.deverdo.instagram.Fragment.HomeFragment;
+import app.deverdo.instagram.Fragment.NotificationFragment;
+import app.deverdo.instagram.Fragment.ProfileFragment;
+import app.deverdo.instagram.Fragment.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        //en başta home fragment acılsın
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                        break;
+
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+
+                            break;
+                        case R.id.nav_add:
+                            selectedFragment = null;
+                            startActivity(new Intent(MainActivity.this,PostActivity.class));
+                            break;
+                        case R.id.nav_heart:
+                            selectedFragment = new NotificationFragment();
+                            break;
+                        case R.id.nav_profile:
+                            SharedPreferences.Editor editor = getSharedPreferences("PREFS",MODE_PRIVATE).edit();
+                            editor.apply();
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    //eğer fragment seçildiyse o fragmente götür.
+                    if (selectedFragment != null){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                selectedFragment).commit();
+                    }
+
+                    return true;
+                }
+            };
 }
